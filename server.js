3155eb7b0cf70f4b9585017e1a7e20a3bd3a1420
Asaju Enitan Michael <asajuenitan@gmail.com>
2021-03-09 00:42:21 +0100

    ✍🏻 UPDATE(Database):

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
-const http = require('http');
+const http = require("http");
 const port = process.env.PORT || 8000;
+const dbUrl = process.env.MONGOURL;
 module.exports = async (app, mongoose) => {
-    try {
-        const server = http.createServer(app);
-        server.listen(port, (err) => {
-            if (err) {
-                console.log(`Error: ${err}`);
-                process.exit(-1);
-            }
-            console.log(`Server running on ${port}`);
-        });
-        await mongoose.connect('mongodb://localhost/movieDbTest', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false,
-            useCreateIndex: true
-        });
+  try {
+    const server = http.createServer(app);
+    server.listen(port, (err) => {
+      if (err) {
+        console.log(`Error: ${err}`);
+        process.exit(-1);
+      }
+      console.log(`Server running on ${port}`);
+    });
+    await mongoose.connect(dbUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    });
 
-        return server;
-    } catch (err) {
-        return new Error('App initialization failed');
-    }
+    return server;
+  } catch (err) {
+    return new Error("App initialization failed");
+  }
 };
